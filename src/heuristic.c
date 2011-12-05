8cc08dd5cde5bbbce06c58fe320947b65b86ae10
Yuri Pirola <yuri.pirola@gmail.com>
2011-12-05 16:03:31 +0100

    PPXH-GR, fixed license.

--- src/heuristic.c
+++ src/heuristic.c
@@ -1,12 +1,28 @@
-/*
-** heuristic.c
-**
-** Made by (Yuri)
-** Login   <yuri@dottxxii-12.dottorato.disco.unimib.it>
-**
-** Started on  Wed Nov 26 17:05:51 2008 Yuri
-** Last update Sun May 12 01:17:25 2002 Speed Blue
-*/
+/**
+* PPXH-GR
+* A heuristic algorithm for the Pure Parsimony Xor Haplotyping problem
+*
+* Copyright (C) 2008 Yuri Pirola <yuri.pirola(-at-)gmail.com>
+*
+* Distributed under the terms of the GNU General Public License (GPL)
+*
+*
+* This file is part of PPXH-GR.
+*
+* PPXH-GR is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* PPXH-GR is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with PPXH-GR. If not, see <http://www.gnu.org/licenses/>.
+*
+**/
 
 #define _GNU_SOURCE
 
